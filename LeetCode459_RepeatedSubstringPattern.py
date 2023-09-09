ebab001778cf7761ecb3c18da48556f43e43d4ba
rirefat <rirefat.official@gmail.com>
2023-09-09 08:16:55 +0600

    adding leetcode 459

---
+++ LeetCode459_RepeatedSubstringPattern.py
@@ -0,0 +1,5 @@
+# https://leetcode.com/problems/repeated-substring-pattern/description/
+class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        ss = (s+s)[1:-1]
+        return s in ss
